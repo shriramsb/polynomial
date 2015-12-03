e0aaf9cc0f2d199ef57300ce90d9723471027d64
Shriram_S_B <sbshriram16@gmail.com>
2015-12-03 22:03:14 +0530

    Added Polynomial class

--- polynomial.h
+++ polynomial.h
@@ -1,1 +1,18 @@
+#include<iostream>
+class Polynomial
+{	
+	private:
+		int n;	//degree of polynomial
+		float *coeff;
+	public:
+		void read();
+		Polynomial operator+(const Polynomial &) const;
+		Polynomial operator-(const Polynomial &) const;
+		Polynomial operator*(const Polynomial &) const;
+		Polynomial operator/(const Polynomial &) const;
+		Polynomial operator%(const Polynomial &) const;
+		float value(const float) const;
+		void print() const;
+		 
 
+};
